a8dce19367df4d879412b3c48a262562df152437
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-08-27 23:54:07 +0700

    LCT GAN

---
+++ ml_utility_loss/lct_gan/main.py
@@ -0,0 +1,247 @@
+from .models.autoencoder import LatentTAE
+from .models.gan import LatentGAN
+from .models.architectures import FCGenerator, FCDiscriminator
+from .utils.evaluation import get_utility_metrics, stat_sim
+from .utils.ctabgan_synthesizer import Condvec
+from sklearn.preprocessing import StandardScaler
+
+import pandas as pd
+import numpy as np
+import pickle
+from tqdm import tqdm
+
+import time
+import torch
+from torch.autograd import Variable
+Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+
+experiment_params = [
+    {
+        "best_ae" : 1000,
+        "embedding_size": 64,
+        "raw_csv_path": "./data/Adult.csv",
+        "test_ratio": 0.20,
+        "categorical_columns": ['workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'gender', 'native-country', 'income'],
+        "log_columns": [],
+        "mixed_columns" : {'capital-loss': [0.0], 'capital-gain': [0.0]},
+        "integer_columns" : ['age', 'fnlwgt', 'capital-gain', 'capital-loss', 'hours-per-week'],
+        "problem_type": {"Classification": 'income'}
+    },
+    {
+        "best_ae": 1000,
+        "embedding_size": 64,
+        "raw_csv_path": "./data/Covtype.csv",
+        "test_ratio": 0.20,
+        "categorical_columns": ['Wilderness_Area1', 'Wilderness_Area2', 'Wilderness_Area3', 'Wilderness_Area4',
+                                'Soil_Type1', 'Soil_Type2', 'Soil_Type3', 'Soil_Type4', 'Soil_Type5',
+                                'Soil_Type6', 'Soil_Type7', 'Soil_Type8', 'Soil_Type9', 'Soil_Type10',
+                                'Soil_Type11', 'Soil_Type12', 'Soil_Type13', 'Soil_Type14',
+                                'Soil_Type15', 'Soil_Type16', 'Soil_Type17', 'Soil_Type18',
+                                'Soil_Type19', 'Soil_Type20', 'Soil_Type21', 'Soil_Type22',
+                                'Soil_Type23', 'Soil_Type24', 'Soil_Type25', 'Soil_Type26',
+                                'Soil_Type27', 'Soil_Type28', 'Soil_Type29', 'Soil_Type30',
+                                'Soil_Type31', 'Soil_Type32', 'Soil_Type33', 'Soil_Type34',
+                                'Soil_Type35', 'Soil_Type36', 'Soil_Type37', 'Soil_Type38',
+                                'Soil_Type39', 'Soil_Type40', 'Cover_Type'],
+        "log_columns": [],
+        "mixed_columns": {"Hillshade_3pm": [0.0]},
+        "integer_columns": ['Elevation', 'Aspect', 'Slope', 'Horizontal_Distance_To_Hydrology',
+                            'Vertical_Distance_To_Hydrology', 'Horizontal_Distance_To_Roadways',
+                            'Hillshade_9am', 'Hillshade_Noon', 'Hillshade_3pm',
+                            'Horizontal_Distance_To_Fire_Points'],
+        "problem_type": {"Classification": "Cover_Type"}
+    },
+    {
+        "best_ae" : 1000,
+        "embedding_size" : 96,
+        "raw_csv_path": "./data/Credit.csv",
+        "test_ratio": 0.20,
+        "categorical_columns": ["Class"],
+        "log_columns": ["Amount"],
+        "mixed_columns": {},
+        "integer_columns": ["Time"],
+        "problem_type": {"Classification": "Class"}
+    },
+    # {
+    #     "best_ae" : 1000,
+    #     "embedding_size" : 96,
+    #     "raw_csv_path": "./data/Intrusion.csv",
+    #     "test_ratio": 0.20,
+    #     "categorical_columns": ['protocol_type', 'service', 'flag', 'land', 'wrong_fragment', 'urgent', 'hot',
+    #                             'num_failed_logins', 'logged_in', 'num_compromised', 'root_shell',
+    #                                 'su_attempted', 'num_root', 'num_file_creations', 'num_shells',
+    #                                 'num_access_files', 'num_outbound_cmds', 'is_host_login',
+    #                                 'is_guest_login', 'class'],
+    #     "log_columns": ["dst_bytes", "src_bytes"],
+    #     "mixed_columns": {},
+    #     "integer_columns": ["duration", "src_bytes", "dst_bytes", "count", "srv_count", "dst_host_count", "dst_host_srv_count"],
+    #     "problem_type": {"Classification": "class"}
+    # },
+    {
+        "best_ae" : 1000,
+        "embedding_size": 32,
+        "raw_csv_path" : "./data/Loan.csv",
+        "test_ratio" : 0.20,
+        "categorical_columns" : ["Family","Education","PersonalLoan","Securities Account","CD Account","Online","CreditCard"], # Note here ZIP Code is treated as numeric.  
+        "log_columns" : [],
+        "mixed_columns": {"Mortgage":[0.0]},
+        "integer_columns": ["Age", "Experience", "Income","Mortgage"],
+        "problem_type": {"Classification": "PersonalLoan"}
+    }
+]
+
+def latent_gan_experiment(
+        bottleneck=64,
+        ae_epochs=1000,
+        ae_batch_size=512,
+        gan_latent_dim=16,
+        gan_epochs=500,
+        gan_n_critic=2,
+        gan_batch_size=512):
+
+    # autoencoder = LatentTAE(embedding_size=bottleneck)
+    # autoencoder.fit(n_epochs=ae_epochs, batch_size=ae_batch_size)
+    # print(f"Autoencoder loss: {autoencoder.loss}")
+    # pickle.dump(autoencoder, open(f"ae_pickles/latent_ae{bottleneck}_{ae_epochs}.pickle", 'wb'))
+
+    for e in experiment_params:
+
+        dataset_path = e["raw_csv_path"]
+        dataset_categories = e["categorical_columns"]
+        best_ae = e["best_ae"]
+        del e["best_ae"]
+
+        pickle_path = "./ae_pickles/" + e['raw_csv_path'].replace("./data/", "").replace(".csv", f"_ae{e['embedding_size']}_{best_ae}.pickle")
+
+        print(f"Opening {pickle_path}")
+        ae_pf = open(pickle_path, 'rb')
+        ae = pickle.load(ae_pf)
+        ae_pf.close()
+
+        # EVALUATING AUTO-ENCODER
+        latent_data = ae.get_latent_dataset() # could be loaded from file
+
+        real_path = e["raw_csv_path"]
+        decoded_path = e["raw_csv_path"].replace("./data/", "./data/decoded/").replace(".csv", f"_decoded{e['embedding_size']}_test.csv")
+
+        reconstructed_data = ae.decode(latent_data, batch=True)
+        reconstructed_data.to_csv(decoded_path, index=False)
+
+        real_path = real_path
+        fake_paths = [ decoded_path ]
+        
+        evaluate(real_path, fake_paths, e["categorical_columns"], ml=False)
+        # END OF EVALUATION
+
+        sscaler = StandardScaler()
+        sscaler.fit(latent_data)
+
+        lat_normalized = sscaler.transform(latent_data)
+        gan = LatentGAN(e["embedding_size"], latent_dim=gan_latent_dim)
+
+        def measure(x):
+            df = gan.sample(len(latent_data), ae, sscaler)
+            df.to_csv(dataset_path.replace(".csv", "_fake.csv"), index=False)
+            print(df)
+            if x == "epoch":
+                res = evaluate(dataset_path, [ dataset_path.replace(".csv", "_fake.csv") ], dataset_categories, ml=False)
+            else: 
+                res = evaluate(dataset_path, [ dataset_path.replace(".csv", "_fake.csv") ], dataset_categories, ml=True)
+
+            pf = open(f"./results/gan/{e['raw_csv_path'].replace('./data/', '').replace('.csv', str(x) + '_results.txt')}", 'a')
+            pf.write(str(res) + "\n")
+            pf.close()
+
+        cb = lambda x : measure(x)
+
+        gan.fit(lat_normalized, ae.train_data, ae.transformer, epochs=gan_epochs, batch_size=gan_batch_size, n_critic=gan_n_critic, callback=cb)
+
+        gan_pf = open("./gan_pickles/" + e['raw_csv_path'].replace("./data/", "").replace(".csv", f"_gan{gan_latent_dim}_{gan_epochs}.pickle"), 'wb')
+        pickle.dump(gan, gan_pf)
+        gan_pf.close()
+
+def ae_experiment():
+
+    epochs = 1000
+    ae_batch_size=512
+    
+    for exp in tqdm(experiment_params):
+
+        best_ae = exp["best_ae"]
+        del exp["best_ae"]
+        
+        print(f"Training on {exp['raw_csv_path']}")
+        start_time = time.time()
+
+        ae = LatentTAE(**exp)
+        ae.fit(n_epochs=epochs, batch_size=ae_batch_size)
+        time_to_train = time.time() - start_time
+        print("--- %s seconds ---" % (time_to_train))
+
+        ae_pf = open("./ae_pickles/" + exp['raw_csv_path'].replace("./data/", "").replace(".csv", f"_ae{exp['embedding_size']}_{epochs}.pickle"), 'wb')
+        pickle.dump(ae, ae_pf)
+        ae_pf.close()
+
+        real_path = exp["raw_csv_path"]
+        decoded_path = exp["raw_csv_path"].replace("./data/", "./data/decoded/").replace(".csv", f"_decoded{exp['embedding_size']}_{epochs}.csv")
+
+        lat_data = ae.get_latent_dataset()
+        reconstructed_data = ae.decode(lat_data, batch=True)
+
+        reconstructed_data.to_csv(decoded_path, index=False)
+
+        real_path = real_path
+        fake_paths = [ decoded_path ]
+
+        res = [ time_to_train, None ]
+
+        try:
+            res = [ time_to_train, evaluate(real_path, fake_paths, exp["categorical_columns"], ml=True) ]
+        except:
+            pass
+
+        pf = open(f"./results/ae/{exp['raw_csv_path'].replace('./data/', '').replace('.csv','_results.txt')}", 'a')
+        pf.write(str(res[0]) + "\n" + str(res[1]) + "\n")
+        pf.close()
+
+        exp["best_ae"] = best_ae
+
+def evaluate(real_path, fake_paths, categorical, ml=True):
+    print("Statistical similarities")
+
+    # Storing and presenting the results as a dataframe
+    stat_res_avg = []
+    for fake_path in fake_paths:
+        stat_res = stat_sim(real_path, fake_path, categorical)
+        stat_res_avg.append(stat_res)
+
+    stat_columns = ["Average WD (Continuous Columns)",
+                    "Average JSD (Categorical Columns)", "Correlation Distance"]
+    stat_results = pd.DataFrame(np.array(stat_res_avg).mean(
+        axis=0).reshape(1, 3), columns=stat_columns)
+    print(stat_results)
+
+    if ml:
+        print("Machine Learning performance")
+        classifiers_list = ["lr", "dt", "rf", "mlp"]
+        # Storing and presenting the results as a dataframe
+        result_df = None
+        try:
+            result_mat = get_utility_metrics(
+                real_path, fake_paths, "MinMax", test_ratio=0.20)
+            result_df = pd.DataFrame(result_mat, columns=["Acc", "AUC", "F1_Score"])
+            result_df.index = classifiers_list
+            print(result_df)
+
+        except:
+            print(f"There was an issue with {real_path}. Probably not enough classes -> low variety.")
+            pass
+
+        return stat_results, result_df
+
+
+    return stat_results
+
+if __name__ == "__main__":
+    ae_experiment()
+    latent_gan_experiment()
---
+++ ml_utility_loss/lct_gan/models/gan.py
@@ -0,0 +1,219 @@
+from cmath import e
+from torch import batch_norm, nn, optim, rand
+from torch.nn import functional as F
+import torch.autograd as autograd
+import random
+
+import numpy as np
+import torch
+from tqdm import tqdm
+import pandas as pd
+import time
+
+from torch.autograd import Variable
+from .architectures import FCDiscriminator, FCGenerator
+from ..utils.ctabgan_synthesizer import Condvec, Sampler, cond_loss
+
+Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+
+class LatentGAN:
+
+    # input_size should be input_size + cond_vector
+    def __init__(self, input_size, latent_dim, minutes=15):
+        self.input_size = input_size
+        self.generator = None
+        self.discriminator = None
+        self.latent_dim = latent_dim
+        self.measurements = []
+        # Loss weight for gradient penalty
+        self.lambda_gp = 10
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    def fit(self, latent_data, original_data, transformer, epochs=5, batch_size=256, lr=0.0002, b1=0.5, b2=0.999, n_critic=5, callback=None):
+        
+        assert len(latent_data) == len(original_data)
+
+        transformer_output_info = transformer.output_info
+        
+        cond_generator = Condvec(original_data, transformer_output_info)
+        data_sampler = Sampler(original_data, transformer_output_info)
+
+        self.cond_generator = cond_generator
+        self.data_sampler = data_sampler
+        self.batch_size = batch_size
+        self.transformer = transformer
+
+        # self.generator = FCGenerator(self.input_size, self.latent_dim + cond_generator.n_opt)
+        self.generator = FCGenerator(self.input_size, self.latent_dim)
+        # self.discriminator = FCDiscriminator(self.input_size + cond_generator.n_opt, batch_size=batch_size)
+        self.discriminator = FCDiscriminator(self.input_size, batch_size=batch_size)
+
+        self.generator.to(self.device)
+        self.discriminator.to(self.device)
+
+        if torch.cuda.is_available():
+            self.generator.cuda()
+            self.discriminator.cuda()
+
+        # Optimizers
+        optimizer_G = torch.optim.Adam(self.generator.parameters(), lr=lr, betas=(b1, b2))
+        optimizer_D = torch.optim.Adam(self.discriminator.parameters(), lr=lr, betas=(b1, b2))
+        
+        steps = (len(latent_data) // batch_size) + 1
+
+        loss_g  = torch.tensor([1.0]) # just for logging purposes, it is reassigned down
+        loss_d = torch.tensor([1.0])  # just for logging purposes, it is reassigned down
+
+        # Experimentation purposes
+
+        start_time = time.time()
+
+        done_15m = False
+        done_30m = False
+        done_60m = False
+
+        epochs = int(1e+9)
+
+        adversarial_loss = torch.nn.BCELoss()
+        valid = Variable(Tensor(batch_size, 1).fill_(1.0), requires_grad=False)
+        non_valid = Variable(Tensor(batch_size, 1).fill_(0.0), requires_grad=False)
+        # Experimentation purposes
+
+        for epoch in tqdm(range(epochs)):
+
+            if epoch % 50 == 0:
+                callback("epoch")
+
+            self.generator.train()
+            self.discriminator.train()
+
+            for i in range(steps):
+
+                ### EXPERIMENTATION
+                elapsed_time = time.time() - start_time 
+
+                if elapsed_time > 15*60 and not done_15m:
+                    callback(15)
+                    done_15m = True
+                elif elapsed_time > 30*60 and not done_30m:
+                    callback(30)
+                    done_30m = True
+                elif elapsed_time > 60*60 and not done_60m:
+                    callback(60)
+                    done_60m = True
+                    return
+                #### EXPERIMENTATION
+                
+                cond_vecs = cond_generator.sample_train(batch_size)
+                c, m, col, opt = cond_vecs
+                c = torch.from_numpy(c).to(self.device)
+                m = torch.from_numpy(m).to(self.device)
+
+                original_idx = data_sampler.sample_idx(batch_size, col, opt)
+                real = latent_data[original_idx]
+                real = torch.from_numpy(real.astype('float32')).to(self.device)
+                
+                ### TRAIN DISCRIMINATOR
+                optimizer_D.zero_grad()
+
+                z = Tensor(np.random.uniform(0, 1, (batch_size, self.latent_dim))).to(self.device)
+                # z = torch.cat([z , c], dim=1).to(self.device)
+                z = torch.cat([z], dim=1).to(self.device)
+                z = Variable(z).to(self.device)
+
+                fake = self.generator(z).to(self.device)
+                
+                # fake_cat_d = torch.cat([fake, c], dim=1).to(self.device).to(self.device)
+                fake_cat_d = torch.cat([fake], dim=1).to(self.device)
+                # real_cat_d = torch.cat([Variable(real.type(Tensor)), c], dim=1).to(self.device)
+                real_cat_d = torch.cat([Variable(real.type(Tensor))], dim=1).to(self.device)
+
+                real_probability = self.discriminator(real_cat_d).to(self.device)
+                fake_probability = self.discriminator(fake_cat_d).to(self.device)
+
+                # Gradient penalty
+                # gradient_penalty = self.compute_gradient_penalty(self.discriminator, real_cat_d.data, fake_cat_d.data)
+                gradient_penalty = 0
+                # Adversarial loss
+                loss_d = -torch.mean(real_probability) + torch.mean(fake_probability) + self.lambda_gp * gradient_penalty
+                # loss_d = (-(torch.log(real_probability).mean()) - (torch.log(1. - fake_probability).mean()))
+
+                # real_loss = adversarial_loss(real_probability, valid)
+                # fake_loss = adversarial_loss(fake_probability, non_valid)
+                # loss_d = (real_loss + fake_loss) / 2
+                loss_d.backward()
+
+                optimizer_D.step()
+
+                ### TRAIN GENERATOR
+                optimizer_G.zero_grad()
+
+                if i % n_critic == 0:
+                        # Generate a batch of data
+                    fake = self.generator(z).to(self.device)
+                    # fake_probability = self.discriminator(torch.cat([fake, c], dim=1).to(self.device))
+                    fake_probability = self.discriminator(torch.cat([fake], dim=1).to(self.device))
+
+                    # loss_g = adversarial_loss(fake_probability, valid)
+                    # loss_g = -(torch.log(fake_probability + 1e-4).mean())
+                    loss_g = -torch.mean(fake_probability)
+                    loss_g.backward()
+
+                    optimizer_G.step()
+
+            print("[Epoch %d/%d] [D loss: %f] [G loss: %f]" 
+                % (epoch + 1, epochs, loss_d.item(), loss_g.item())
+            )
+
+        callback(None)
+
+    def compute_gradient_penalty(self, D, real_samples, fake_samples):
+
+        """Calculates the gradient penalty loss for WGAN GP"""
+
+        # Random weight term for interpolation between real and fake samples
+        alpha = Tensor(np.random.random((real_samples.size(0), 1))).to(self.device)
+        # Get random interpolation between real and fake samples
+        interpolates = (alpha * real_samples + ((1 - alpha) * fake_samples)).requires_grad_(True).to(self.device)
+        d_interpolates = D(interpolates)
+
+        fake = Variable(Tensor(real_samples.shape[0], 1).fill_(1.0), requires_grad=False).to(self.device)
+        # Get gradient w.r.t. interpolates
+        gradients = autograd.grad(
+            outputs=d_interpolates,
+            inputs=interpolates,
+            grad_outputs=fake,
+            create_graph=True,
+            retain_graph=True,
+            only_inputs=True,
+        )[0]
+        gradients = gradients.view(gradients.size(0), -1)
+        gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
+
+        return gradient_penalty
+
+    def sample(self, n, decoder, scaler):
+        
+        # turning the generator into inference mode to effectively use running statistics in batch norm layers
+        self.generator.eval()
+        
+        # generating synthetic data in batches accordingly to the total no. required
+        steps = (n // self.batch_size) + 1
+        data = []
+        print("Number of steps: " + str(steps))
+        for _ in tqdm(range(steps)):
+            # generating synthetic data using sampled noise and conditional vectors
+            ### Generating a batch
+            z = Tensor(np.random.uniform(0, 1, (self.batch_size, self.latent_dim)))
+            z_cond = Tensor(self.cond_generator.sample(self.batch_size))
+            # z = torch.cat([z, z_cond], dim=1).to(self.device)
+            z = torch.cat([z], dim=1).to(self.device)
+
+            fake = self.generator(z).cpu().detach().numpy()
+            decoded = decoder.decode(scaler.inverse_transform(fake), batch=True)
+            data.append(decoded)
+
+        data = pd.concat(data)
+        print("Sampled data length")
+        print(len(data))
+        return data[0:n]
