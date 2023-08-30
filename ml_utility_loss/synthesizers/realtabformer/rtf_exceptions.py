a839b646b0256c26eb6ab1a36d445e4eef1dab13
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-08-30 18:46:57 +0700

    Update rtf_exceptions.py

--- ml_utility_loss/synthesizers/realtabformer/rtf_exceptions.py
+++ ml_utility_loss/synthesizers/realtabformer/rtf_exceptions.py
@@ -1,4 +1,20 @@
+class SampleEmptyError(Exception):
+    """Exception raised for generated samples without valid observations.
 
+    Attributes:
+        salary -- input salary which caused the error
+        message -- explanation of the error
+    """
+
+    def __init__(
+        self, message="Generated sample is empty after validation.", in_size=None
+    ):
+        self.in_size = in_size
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f"{self.in_size} -> {self.message}"
 
 class SampleEmptyLimitError(SampleEmptyError):
     """Exception raised when SampleEmptyError is raised
