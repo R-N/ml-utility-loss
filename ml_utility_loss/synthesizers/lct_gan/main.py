607c39609af66a2592fddd07799f5998e48e83ce
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-09-11 00:50:28 +0700

    Update main.py

--- ml_utility_loss/synthesizers/lct_gan/main.py
+++ ml_utility_loss/synthesizers/lct_gan/main.py
@@ -11,7 +11,7 @@ Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTen
 
 experiment_params = [
     {
-        "best_ae" : 1000,
+        "best_ae" : 1, # steps
         "embedding_size": 64,
         "raw_csv_path": "./data/Adult.csv",
         "test_ratio": 0.20,
@@ -21,52 +21,6 @@ experiment_params = [
         "integer_columns" : ['age', 'fnlwgt', 'capital-gain', 'capital-loss', 'hours-per-week'],
         "problem_type": {"Classification": 'income'}
     },
-    {
-        "best_ae": 1000,
-        "embedding_size": 64,
-        "raw_csv_path": "./data/Covtype.csv",
-        "test_ratio": 0.20,
-        "categorical_columns": ['Wilderness_Area1', 'Wilderness_Area2', 'Wilderness_Area3', 'Wilderness_Area4',
-                                'Soil_Type1', 'Soil_Type2', 'Soil_Type3', 'Soil_Type4', 'Soil_Type5',
-                                'Soil_Type6', 'Soil_Type7', 'Soil_Type8', 'Soil_Type9', 'Soil_Type10',
-                                'Soil_Type11', 'Soil_Type12', 'Soil_Type13', 'Soil_Type14',
-                                'Soil_Type15', 'Soil_Type16', 'Soil_Type17', 'Soil_Type18',
-                                'Soil_Type19', 'Soil_Type20', 'Soil_Type21', 'Soil_Type22',
-                                'Soil_Type23', 'Soil_Type24', 'Soil_Type25', 'Soil_Type26',
-                                'Soil_Type27', 'Soil_Type28', 'Soil_Type29', 'Soil_Type30',
-                                'Soil_Type31', 'Soil_Type32', 'Soil_Type33', 'Soil_Type34',
-                                'Soil_Type35', 'Soil_Type36', 'Soil_Type37', 'Soil_Type38',
-                                'Soil_Type39', 'Soil_Type40', 'Cover_Type'],
-        "log_columns": [],
-        "mixed_columns": {"Hillshade_3pm": [0.0]},
-        "integer_columns": ['Elevation', 'Aspect', 'Slope', 'Horizontal_Distance_To_Hydrology',
-                            'Vertical_Distance_To_Hydrology', 'Horizontal_Distance_To_Roadways',
-                            'Hillshade_9am', 'Hillshade_Noon', 'Hillshade_3pm',
-                            'Horizontal_Distance_To_Fire_Points'],
-        "problem_type": {"Classification": "Cover_Type"}
-    },
-    {
-        "best_ae" : 1000,
-        "embedding_size" : 96,
-        "raw_csv_path": "./data/Credit.csv",
-        "test_ratio": 0.20,
-        "categorical_columns": ["Class"],
-        "log_columns": ["Amount"],
-        "mixed_columns": {},
-        "integer_columns": ["Time"],
-        "problem_type": {"Classification": "Class"}
-    },
-    {
-        "best_ae" : 1000,
-        "embedding_size": 32,
-        "raw_csv_path" : "./data/Loan.csv",
-        "test_ratio" : 0.20,
-        "categorical_columns" : ["Family","Education","PersonalLoan","Securities Account","CD Account","Online","CreditCard"], # Note here ZIP Code is treated as numeric.  
-        "log_columns" : [],
-        "mixed_columns": {"Mortgage":[0.0]},
-        "integer_columns": ["Age", "Experience", "Income","Mortgage"],
-        "problem_type": {"Classification": "PersonalLoan"}
-    }
 ]
 
 def latent_gan_experiment(
