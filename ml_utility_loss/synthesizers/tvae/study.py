6bbd41987c058f9e20a02fd006beb01034b8afd8
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-09-17 16:17:13 +0700

    TVAE tuning

---
+++ ml_utility_loss/synthesizers/tvae/study.py
@@ -0,0 +1,41 @@
+
+from .wrapper import TVAE
+from ...loss_learning.pipeline import eval_ml_utility
+
+PARAM_SPACE = {
+    "embedding_dim": ("int_exp_2", 128, 512),
+    "compress_dims": ("int_exp_2", 32, 256),
+    "compress_depth": ("int", 1, 4),
+    "decompress_dims": ("int_exp_2", 32, 256),
+    "decompress_depth": ("int", 1, 4),
+    "l2scale": ("log_float", 1e-6, 1e-4),
+    "batch_size": ("int_exp_2", 32, 512),
+    "epochs": ("int", 100, 1000),
+    "loss_factor": ("float", 0.5, 3.0),
+}
+
+def objective(
+    datasets,
+    task,
+    cat_features,
+    ml_utility_params={},
+    checkpoint_dir=None,
+    log_dir=None,
+    trial=None,
+    **kwargs
+):
+    train, test = datasets
+
+    tvae = TVAE(**kwargs)
+    tvae.fit(train, cat_features)
+
+    # Create synthetic data
+    synth = tvae.sample(len(train))
+
+    value = eval_ml_utility(
+        (synth, test),
+        task,
+        **ml_utility_params
+    )
+
+    return value
