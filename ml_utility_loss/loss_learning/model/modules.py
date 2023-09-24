69b5ecb9e14f4a13d86d7076c357ae1f47748eac
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-09-24 17:40:27 +0700

    Update modules.py

--- ml_utility_loss/loss_learning/model/modules.py
+++ ml_utility_loss/loss_learning/model/modules.py
@@ -138,10 +138,10 @@ class InducedSetAttention(nn.Module):
         # Ok so this is actually a problem
         # It expects batched input so I is repeated to the batch dimension
         # So it has to be handled
-        print("ISAB", q.shape)
         I = self.I.unsqueeze(0).repeat(q.size(0), 1, 1) if q.dim() > 2 else self.I
-        print("ISAB I", I.shape)
+        print("ISAB", I.shape, q.shape, k.shape, v.shape)
         H, I_attn = self.mab0(I, k, v, mask=None) #yes it's none
+        print("ISAB H", H.shape)
         O, O_attn = self.mab1(q, H, H, mask=mask)
         return O, (I_attn, O_attn)
     
