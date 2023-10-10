daf0c3cb1a49fc70fa423b2403d5264059ee2acb
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-10-11 02:58:38 +0700

    Update data_utils.py

--- ml_utility_loss/synthesizers/realtabformer/data_utils.py
+++ ml_utility_loss/synthesizers/realtabformer/data_utils.py
@@ -216,11 +216,11 @@ def process_numeric_data(
                 zfill += 1
             transform_data["zfill"] = int(zfill)
 
-        """
-        if "charges" in series.name:
+        #"""
+        if "leucocyte" in series.name:
             print("min B", series.astype(int).min())
             print("zfill", zfill)
-        """
+        #"""
         series = series.str.zfill(zfill)
     else:
         # Make sure that we don't exessively truncate the data.
@@ -368,10 +368,10 @@ def tokenize_numeric_col(series: pd.Series, nparts=2, col_zfill=2, has_negative=
 
     tr.columns = encode_partition_numeric_col(col, tr, col_zfill)
 
-    """
-    if col == "6___NUMERIC___charges":
+    #"""
+    if col == "03___NUMERIC___leucocyte":
         print("tr", tr.sort_values(list(tr.columns[::-1]), ascending=False).head())
-    """
+    #"""
 
     return tr
 
@@ -536,10 +536,10 @@ def process_data(
 
     processed_df = pd.concat([pd.DataFrame()] + processed_series, axis=1)
     #print("processed_df A", processed_df.head())
-    """
-    print("min A", processed_df["6___NUMERIC___charges"].astype(float).min())
-    print("processed_df A", processed_df[["6___NUMERIC___charges"]].sort_values("6___NUMERIC___charges", ascending=True).head())
-    """
+    #"""
+    print("min A", processed_df["03___NUMERIC___leucocyte"].astype(float).min())
+    print("processed_df A", processed_df[["03___NUMERIC___leucocyte"]].sort_values("03___NUMERIC___leucocyte", ascending=True).head())
+    #"""
 
     if not processed_df.empty:
         # Tokenize the processed numeric and datetime data.
@@ -556,12 +556,12 @@ def process_data(
             axis=1,
         )
     #print("processed_df B", processed_df.head())
-    """
-    if "6___NUMERIC___charges_10" in processed_df.columns:
-        print("max B", processed_df["6___NUMERIC___charges_10"].sort_values(ascending=False).iloc[0])
-    _cols = [x for x in processed_df.columns if "6___NUMERIC___charges" in x]
+    #"""
+    if "03___NUMERIC___leucocyte_07" in processed_df.columns:
+        print("max B", processed_df["03___NUMERIC___leucocyte_07"].sort_values(ascending=False).iloc[0])
+    _cols = [x for x in processed_df.columns if "03___NUMERIC___leucocyte" in x]
     print("processed_df B", processed_df[_cols].sort_values(list(_cols[::-1]), ascending=False).head())
-    """
+    #"""
 
     # NOTE: The categorical data should be the last to be processed!
     categorical_cols = df.columns.difference(numeric_cols).difference(datetime_cols)
