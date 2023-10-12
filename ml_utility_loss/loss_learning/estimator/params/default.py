7fd0ca5bb929534d73c6e82c6d9dc88bdc72c9c2
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-10-12 19:27:44 +0700

    Update default.py

--- ml_utility_loss/loss_learning/estimator/params/default.py
+++ ml_utility_loss/loss_learning/estimator/params/default.py
@@ -30,7 +30,7 @@ PARAM_SPACE = {
         "AVERAGE_MUL"
     ]),
     # Common model args
-    "d_model": ("int_exp_2", 8, 128), 
+    "d_model": ("int_exp_2", 8, 64), 
     "dropout": ("float", 0.0, 0.2), 
     "softmax": ("softmax", ["softmax", "relu15"]),
     "flip": BOOLEAN,
@@ -38,20 +38,20 @@ PARAM_SPACE = {
     "loss_clamp": ("log_float", 0.5, 10.0),
     # Transformer args
     "tf_num_inds": ("int_exp_2", 8, 64),
-    "tf_d_inner": ("int_exp_2", 32, 128),
-    "tf_n_layers": ("int", 2, 6), 
+    "tf_d_inner": ("int_exp_2", 32, 64),
+    "tf_n_layers": ("int", 2, 4), 
     "tf_n_head": ("int_exp_2", 2, 16), 
     "tf_activation": ("activation", ["relu", "gelu", "identity"]),
     # Transformer PMA args
     "tf_pma": ("conditional", {
         "tf_pma_start": ("int", -4, -1),
-        "tf_pma_high": ("int_exp_2", 8, 512),
+        "tf_pma_high": ("int_exp_2", 8, 64),
         "tf_pma_low": ("int_exp_2", 2, 32),
     }),
     "tf_share_ffn": BOOLEAN,
     # Adapter args
-    "ada_d_hid": ("int_exp_2", 8, 64), 
-    "ada_n_layers": ("int", 2, 8), 
+    "ada_d_hid": ("int_exp_2", 8, 128), 
+    "ada_n_layers": ("int", 2, 4), 
     "ada_activation": ("activation", [
         "tanh", "sigmoid", 
         "relu", "leakyrelu", 
@@ -59,9 +59,9 @@ PARAM_SPACE = {
         "identity"
     ]),
     # Head args
-    "head_n_seeds": ("int_exp_2", 1, 8),
-    "head_d_hid": ("int", 8, 128), 
-    "head_n_layers": ("int", 2, 8), 
+    "head_n_seeds": ("int", 1, 8),
+    "head_d_hid": ("int_exp_2", 8, 128), 
+    "head_n_layers": ("int", 2, 4), 
     "head_n_head": ("int_exp_2", 2, 16),
     "head_activation": ("activation", [
         "tanh", "sigmoid", 
@@ -76,7 +76,7 @@ PARAM_SPACE_2 = {
     "dataset_size_high": ("int_exp_2", 256, 4096),
     "batch_size_low": ("int_exp_2", 2, 4),
     "batch_size_high": ("int_exp_2", 4, 16),
-    "patience": ("log_int", 2, 10)
+    "patience": ("log_int", 2, 5)
 }
 
 def update_param_space(param_space, dataset_sizes):
