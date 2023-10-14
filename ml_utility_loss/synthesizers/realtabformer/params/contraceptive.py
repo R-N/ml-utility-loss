42ac13bc5b85b4ee8cc43860b0817b52b994502e
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-10-14 12:28:03 +0700

    Realtabformer params

---
+++ ml_utility_loss/synthesizers/realtabformer/params/contraceptive.py
@@ -0,0 +1,27 @@
+
+
+BEST = {
+    "vocab_size": 32337, 
+    "n_positions_exp_2": 1024, #10, 
+    "n_embd": 672, 
+    "n_layer": 11, 
+    "n_head": 8, #3,
+    "activation_function": "silu", 
+    "resid_pdrop": 0.17416105712363344, 
+    "embd_pdrop": 0.1766300943914589, 
+    "attn_pdrop": 0.05446321185948769, 
+    "layer_norm_epsilon": 2.191547933311004e-06, 
+    "initializer_range": 0.03869076446437605, 
+    "scale_attn_weights": True, 
+    "scale_attn_by_inverse_layer_idx": True, 
+    "epochs": 653, 
+    "batch_size": 32, #5, 
+    "mask_rate": 0.09443299792821876, 
+    "numeric_nparts": 1, 
+    "numeric_precision": 5, 
+    "numeric_max_len": 12, 
+    "evaluation_strategy": "steps", 
+    "gradient_accumulation_steps": 1, #0, 
+    "optim": "adamw_hf", 
+    "num_bootstrap": 73
+}
---
+++ ml_utility_loss/synthesizers/realtabformer/params/treatment.py
@@ -0,0 +1,29 @@
+
+BEST = {
+    "vocab_size": 40213, 
+    "n_positions": 2048, #11, 
+    "n_embd": 704, 
+    "n_layer": 10, 
+    "n_head": 16, #4, 
+    "activation_function": "relu", 
+    "resid_pdrop": 0.10388100829113714, 
+    "embd_pdrop": 0.04133528738742756, 
+    "attn_pdrop": 0.04494868239692987, 
+    "layer_norm_epsilon": 1.426142602358904e-05, 
+    "initializer_range": 0.014157782931728218, 
+    "scale_attn_weights": False, 
+    "scale_attn_by_inverse_layer_idx": True, 
+    "epochs": 194, 
+    "batch_size": 4, #2, 
+    "mask_rate": 0.15236501309370373, 
+    "numeric_nparts": 2, 
+    "numeric_precision": 3, 
+    "numeric_max_len": 13, 
+    "evaluation_strategy": "steps", 
+    "gradient_accumulation_steps": 1, #0, 
+    "optim": "adamw_torch", 
+    "num_bootstrap": 19
+}
+
+# I mean it was just one trial lol
+BEST = {}
