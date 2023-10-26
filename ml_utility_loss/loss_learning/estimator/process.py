10b0a1a804a8dd176633e959eaca8390091c0171
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-10-27 01:41:21 +0700

    Update process.py

--- ml_utility_loss/loss_learning/estimator/process.py
+++ ml_utility_loss/loss_learning/estimator/process.py
@@ -167,7 +167,7 @@ def train_epoch(
     inverse_avg_non_role_model_m=True,
     gradient_penalty=True,
     loss_clamp=2.0,
-    grad_clip=4.0,
+    grad_clip=1.0,
     models = None,
     head="mlu",
     eps=1e-6,
