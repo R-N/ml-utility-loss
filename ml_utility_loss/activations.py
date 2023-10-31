38f2bd292368b2ad45d261a447dcd351dff75185
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-10-31 14:41:15 +0700

    Learnable leaky relu

--- ml_utility_loss/activations.py
+++ ml_utility_loss/activations.py
@@ -1,24 +1,39 @@
 
 from entmax import sparsemax, entmax15, Sparsemax, Entmax15
 from alpharelu import relu15, ReLU15
+from torch import nn
+import torch
 
 class AlphaSigmoid(nn.Module):
-    def __init__(self, F=nn.Sigmoid):
+    def __init__(self, default=1.0, F=nn.Sigmoid, **kwargs):
         super().__init__()
-        self.alpha = nn.Parameter(torch.Tensor([1]).squeeze())
-        self.sig = F()
+        self.alpha = nn.Parameter(torch.Tensor([default]).squeeze())
+        self.f = F(**kwargs)
 
     def forward(self, x):
-        return self.alpha * self.sig(x / self.alpha)
+        return self.alpha * self.f(x / self.alpha)
 
 class AlphaTanh(AlphaSigmoid):
-    def __init__(self):
-        super().__init__(F=nn.Tanh)
+    def __init__(self, **kwargs):
+        super().__init__(F=nn.Tanh, **kwargs)
 
 class AlphaReLU15(AlphaSigmoid):
-    def __init__(self):
-        super().__init__(F=ReLU15)
+    def __init__(self, **kwargs):
+        super().__init__(F=ReLU15, **kwargs)
 
 class AlphaSoftmax(AlphaSigmoid):
-    def __init__(self):
-        super().__init__(F=nn.Softmax)
+    def __init__(self, **kwargs):
+        super().__init__(F=nn.Softmax, **kwargs)
+
+class LearnableLeakyReLU(nn.Module):
+    def __init__(self, default=0.01, F=nn.LeakyReLU, **kwargs):
+        self.alpha = nn.Parameter(torch.Tensor([default]).squeeze())
+        self.f = F(**kwargs)
+        
+    @property
+    def negative_slope(self):
+        return self.alpha.item()
+
+    def forward(self, x):
+        return self.f(x)
+    
