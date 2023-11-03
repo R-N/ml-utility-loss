8cd1af2ee6e17db7c457d87b27479560409dbbda
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-11-03 16:07:52 +0700

    Try transformer params from set transformer

--- transformer_debug/transformer/Models.py
+++ transformer_debug/transformer/Models.py
@@ -72,6 +72,8 @@ class Encoder(nn.Module):
                 init=False,
                 softmax=nn.Softmax,
                 activation=nn.ReLU,
+                attn_activation=nn.ReLU,
+                attn_residual=True,
             ) for _ in range(n_layers)
         ])
         self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
@@ -128,6 +130,8 @@ class Decoder(nn.Module):
                 init=False,
                 softmax=nn.Softmax,
                 activation=nn.ReLU,
+                attn_activation=nn.ReLU,
+                attn_residual=True,
             ) for _ in range(n_layers)
         ])
         self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
