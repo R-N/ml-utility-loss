a1cad4bf86a7087445a40dc4384b002d16e57a15
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-11-03 18:06:58 +0700

    Update Models.py

--- transformer_debug/transformer/Models.py
+++ transformer_debug/transformer/Models.py
@@ -72,8 +72,8 @@ class Encoder(nn.Module):
                 init=False,
                 softmax=nn.Softmax,
                 activation=nn.ReLU,
-                attn_activation=None,
-                attn_residual=True,
+                attn_activation=nn.ReLU,
+                attn_residual=False,
             ) for _ in range(n_layers)
         ])
         self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
@@ -130,8 +130,8 @@ class Decoder(nn.Module):
                 init=False,
                 softmax=nn.Softmax,
                 activation=nn.ReLU,
-                attn_activation=None,
-                attn_residual=True,
+                attn_activation=nn.ReLU,
+                attn_residual=False,
             ) for _ in range(n_layers)
         ])
         self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
