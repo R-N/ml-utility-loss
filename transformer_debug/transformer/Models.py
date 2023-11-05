22809cd6fbecc4fd596241eb182862363ca9d41b
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-11-06 01:23:19 +0700

    Update Models.py

--- transformer_debug/transformer/Models.py
+++ transformer_debug/transformer/Models.py
@@ -14,11 +14,12 @@ def get_pad_mask(seq, pad_idx):
     return (seq != pad_idx).unsqueeze(-2)
 
 
-def get_subsequent_mask(seq):
+def get_subsequent_mask(seq, num_inds=0):
     ''' For masking out the subsequent info. '''
     sz_b, len_s = seq.size()
+    num_inds = num_inds or len_s
     subsequent_mask = (1 - torch.triu(
-        torch.ones((1, len_s, len_s), device=seq.device), diagonal=1)).bool()
+        torch.ones((1, len_s, num_inds), device=seq.device), diagonal=1)).bool()
     return subsequent_mask
 
 
