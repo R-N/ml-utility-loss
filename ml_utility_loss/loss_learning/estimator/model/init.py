029994e2845c7cbc1ea70e10dbb67d6ae90f553f
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-11-06 19:21:56 +0700

    Update init.py

--- ml_utility_loss/loss_learning/estimator/model/init.py
+++ ml_utility_loss/loss_learning/estimator/model/init.py
@@ -37,11 +37,11 @@ def init_layer_norm(norm, activation=None):
         torch.nn.init.zeros_(norm.bias)
 
 def init_induction_point(tensor, activation=None):
-    torch.nn.init.xavier_uniform_(tensor, gain=0.1)
+    #torch.nn.init.xavier_uniform_(tensor, gain=0.1)
     #torch.nn.init.uniform_(tensor, -1, 1)
     #torch.nn.init.ones_(tensor)
     #torch.nn.init.orthogonal_(tensor)
-    #torch.nn.init.uniform_(tensor, -0.001, 0.001) # fixnorm
+    torch.nn.init.uniform_(tensor, -0.01, 0.01) # fixnorm
     #torch.nn.init.normal_(tensor, std=0.01) # fixnorm
     elements = math.prod(list(tensor.shape))
     fill = math.sqrt(elements)
