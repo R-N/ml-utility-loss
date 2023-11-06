f7ef1957102c639d82fc0aa349a7f0ad9ee856f4
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-11-06 20:48:55 +0700

    Update init.py

--- ml_utility_loss/loss_learning/estimator/model/init.py
+++ ml_utility_loss/loss_learning/estimator/model/init.py
@@ -23,7 +23,12 @@ def init_attn(linear, activation=None):
         return
     if hasattr(linear, "init"):
         return linear.init(activation=activation)
-    torch.nn.init.xavier_uniform_(linear.weight)
+    a = 0
+    if activation and hasattr(activation, "negative_slope"):
+        a = activation.negative_slope
+    t = type(activation) if isinstance(activation, torch.nn.Module) else activation
+    nonlinearity = ACTIVATIONS_INVERSE[t]
+    torch.nn.init.kaiming_uniform_(linear.weight, a=math.sqrt(5)*a, nonlinearity=nonlinearity)
     if linear.bias is not None:
         torch.nn.init.zeros_(linear.bias)
 
