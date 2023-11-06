9ed82eb95fdaaa722e8409027323a5703554586a
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-11-06 10:10:07 +0700

    Update process_simple.py

--- ml_utility_loss/loss_learning/estimator/process_simple.py
+++ ml_utility_loss/loss_learning/estimator/process_simple.py
@@ -149,6 +149,7 @@ def train_epoch(
         if batch == 0:
             loss_balancer.pre_weigh(*batch_loss)
         batch_loss = sum(loss_balancer(*batch_loss))
+        print(batch_loss.shape)
         if not val:
             if reduction == torch.sum:
                 (batch_loss/batch_size).backward()
