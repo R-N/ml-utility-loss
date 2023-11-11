bd00f8e79873bf3d5e3dc720ac806f44f66c51b5
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-11-11 16:30:13 +0700

    Update process.py

--- ml_utility_loss/loss_learning/estimator/process.py
+++ ml_utility_loss/loss_learning/estimator/process.py
@@ -464,7 +464,8 @@ def calc_g_loss(
     error = error.detach()
     # The gradient is of shape (batch, size, dim)
     # Sum gradient over the size dimension, resulting in (batch, dim)
-    assert dbody_dx.dim() > 2
+    print("gradient shape", dbody_dx.shape)
+    assert dbody_dx.dim() > 2, f"gradient dim too small: {dbody_dx.shape}"
     dbody_dx = torch.sum(dbody_dx, dim=-2)
     assert dbody_dx.dim() == 2 and error.dim() == 1 and len(dbody_dx) == len(error)
 
