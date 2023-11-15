7489e572b761026072bc0e0e2860071199265966
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-11-15 19:18:26 +0700

    Update layers.py

--- ml_utility_loss/loss_learning/estimator/model/layers.py
+++ ml_utility_loss/loss_learning/estimator/model/layers.py
@@ -274,7 +274,6 @@ class DecoderLayer(EncoderLayer):
         pma_attn = None
         if self.pma:
             dec_output, pma_attn = self.pma(dec_output)
-            print("DID PMA")
             if self.pos_ffn_pma:
                 dec_output = self.pos_ffn_pma(dec_output)
         return dec_output, (dec_slf_attn, dec_enc_attn, pma_attn)
