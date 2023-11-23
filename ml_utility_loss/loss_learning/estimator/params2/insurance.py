0d5b299bdf3d64c3ae7dadde274b02cec7bf1e40
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-11-23 20:38:05 +0700

    fix batch size

--- ml_utility_loss/loss_learning/estimator/params2/insurance.py
+++ ml_utility_loss/loss_learning/estimator/params2/insurance.py
@@ -286,8 +286,8 @@ PARAM_SPACE_2 = {
     #"dataset_size_high": ("int_exp_2", 2048, 2048),
     "dataset_size_low": ("int_exp_2", 256, 1024),
     "dataset_size_high": ("int_exp_2", 1024, 2048), # param must exist
-    "batch_size_low": ("int_exp_2", 4, 16),
-    "batch_size_high": ("int_exp_2", 4, 4),
+    "batch_size_low": ("int_exp_2", 4, 4),
+    "batch_size_high": ("int_exp_2", 4, 16),
     "scheduler_patience": ("log_int", 50, 90),
 }
 
