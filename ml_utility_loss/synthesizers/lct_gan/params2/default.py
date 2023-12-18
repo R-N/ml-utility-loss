bced2452db2d18ab55f064681f81aabe61ede24c
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-12-19 00:44:42 +0700

    Update default.py

--- ml_utility_loss/synthesizers/lct_gan/params2/default.py
+++ ml_utility_loss/synthesizers/lct_gan/params2/default.py
@@ -2,7 +2,7 @@ PARAM_SPACE = {
     "n_samples": ("int_exp_2", 16, 512),
     #"sample_batch_size": ("int_exp_2", 16, 512),
     "t_steps": ("int", 1, 16),
-    "target": ("categorical", [None, 1.0]),
+    "mlu_target": ("categorical", [None, 1.0]),
     "n_steps": ("int", 1, 16),
     "loss_fn": ("loss", [
         "mse",
