8efe4236ce8ba3286f8989516ee9528d6ad6a967
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-12-19 01:18:39 +0700

    Update modules.py

--- ml_utility_loss/synthesizers/lct_gan/modules.py
+++ ml_utility_loss/synthesizers/lct_gan/modules.py
@@ -23,7 +23,6 @@ class FCDecoder(nn.Module):
         self.fc6 = nn.Linear(128, input_size)
 
     def forward(self, z):
-        print(z.dtype, self.fc4.weight.dtype)
         h3 = F.relu(self.fc4(z))
         h4 = F.relu(self.fc5(h3))
         return torch.sigmoid(self.fc6(h4))
