07968de1b90e5766212c5065703bdf04c7f9e7a4
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-12-30 15:42:44 +0700

    Update evaluation.py

--- ml_utility_loss/evaluation.py
+++ ml_utility_loss/evaluation.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas as pd 
 from sklearn import metrics
-from sklearn.preprocessing import MinMaxScaler,StandardScaler
+from sklearn.preprocessing import MinMaxScaler,StandardScaler, OneHotEncoder
 from dython.nominal import associations
 from scipy.stats import wasserstein_distance
 from scipy.spatial import distance
+from sklearn.metrics import pairwise_distances
 
 def stat_sim(real_path,fake_path,cat_cols=None):
     
@@ -22,9 +23,13 @@ def stat_sim(real_path,fake_path,cat_cols=None):
   across categorical columns and euclidean norm of the difference in pair-wise correlations between real and synthetic datasets
     
   """
+  Stat_dict={}
   # Loading real and synthetic data
   real = pd.read_csv(real_path)
   fake = pd.read_csv(fake_path)
+
+  really = real.copy()
+  fakey = fake.copy()
 
   # Computing the real and synthetic pair-wise correlations
   real_corr = associations(real, nominal_columns=cat_cols, compute_only=True)['corr']
@@ -42,9 +47,9 @@ def stat_sim(real_path,fake_path,cat_cols=None):
     if column in cat_cols:
 
       # Computing the real and synthetic probabibility mass distributions (pmf) for each categorical column
-      real_pmf=(real[column].value_counts()/real[column].value_counts().sum())
-      fake_pmf=(fake[column].value_counts()/fake[column].value_counts().sum())
-      categories = (fake[column].value_counts()/fake[column].value_counts().sum()).keys().tolist()
+      real_pmf=(really[column].value_counts()/really[column].value_counts().sum())
+      fake_pmf=(fakey[column].value_counts()/fakey[column].value_counts().sum())
+      categories = (fakey[column].value_counts()/fakey[column].value_counts().sum()).keys().tolist()
             
       # Ensuring the pmfs of real and synthetic data have the categories within a column in the same order
       sorted_categories = sorted(categories)
@@ -58,14 +63,14 @@ def stat_sim(real_path,fake_path,cat_cols=None):
             
       # If a category of a column is not generated in the synthetic dataset, pmf of zero is assigned
       if len(real_pmf)!=len(fake_pmf):
-        zero_cats = set(real[column].value_counts().keys())-set(fake[column].value_counts().keys())
+        zero_cats = set(really[column].value_counts().keys())-set(fakey[column].value_counts().keys())
         for z in zero_cats:
           real_pmf_ordered.append(real_pmf[z])
           fake_pmf_ordered.append(0)
 
       # Computing the statistical similarity between real and synthetic pmfs 
-      cat_stat.append(distance.jensenshannon(real_pmf_ordered,fake_pmf_ordered, 2.0))        
-        
+      Stat_dict[column]=(distance.jensenshannon(real_pmf_ordered,fake_pmf_ordered, 2.0))
+      cat_stat.append(Stat_dict[column])  
     else:
       # Scaling the real and synthetic numerical column values between 0 and 1 to obtained normalized statistical similarity
       scaler = MinMaxScaler()
@@ -74,7 +79,8 @@ def stat_sim(real_path,fake_path,cat_cols=None):
       l2 = scaler.transform(fake[column].values.reshape(-1,1)).flatten()
             
       # Computing the statistical similarity between scaled real and synthetic numerical distributions 
-      num_stat.append(wasserstein_distance(l1,l2))
+      Stat_dict[column]= (wasserstein_distance(l1,l2))
+      num_stat.append(Stat_dict[column])
 
   return [np.mean(num_stat),np.mean(cat_stat),corr_dist]
 
@@ -147,3 +153,64 @@ def privacy_metrics(real_path,fake_path,data_percent=15):
   nn_fifth_perc_ff = np.percentile(nn_ratio_ff,5)
     
   return np.array([fifth_perc_rf,fifth_perc_rr,fifth_perc_ff,nn_fifth_perc_rf,nn_fifth_perc_rr,nn_fifth_perc_ff]).reshape(1,6) 
+
+
+def privacy_metrics_2(real_data, fake_data, task_type, data_percent=15):
+
+    """
+    Returns privacy metrics
+    
+    Inputs:
+    3) data_percent -> percentage of data to be sampled from real and synthetic datasets for computing privacy metrics
+    Outputs:
+    1) List containing the 5th percentile distance to closest record (DCR) between real and synthetic as well as within real and synthetic datasets
+    along with 5th percentile of nearest neighbour distance ratio (NNDR) between real and synthetic as well as within real and synthetic datasets
+    
+    """
+    X_num_real, X_cat_real, y_real = real_data
+    X_num_fake, X_cat_fake, y_fake = fake_data
+
+    if task_type == 'regression':
+        X_num_real = np.concatenate([X_num_real, y_real[:, np.newaxis]], axis=1)
+        X_num_fake = np.concatenate([X_num_fake, y_fake[:, np.newaxis]], axis=1)
+    else:
+        if X_cat_fake is None:
+            X_cat_real = y_real[:, np.newaxis].astype(int).astype(str)
+            X_cat_fake = y_fake[:, np.newaxis].astype(int).astype(str)
+        else:
+            X_cat_real = np.concatenate([X_cat_real, y_real[:, np.newaxis].astype(int).astype(str)], axis=1)
+            X_cat_fake = np.concatenate([X_cat_fake, y_fake[:, np.newaxis].astype(int).astype(str)], axis=1)
+
+    if len(y_real) > 50000:
+        ixs = np.random.choice(len(y_real), 50000, replace=False)
+        X_num_real = X_num_real[ixs]
+        X_cat_real = X_cat_real[ixs] if X_cat_real is not None else None
+    
+    if len(y_fake) > 50000:
+        ixs = np.random.choice(len(y_fake), 50000, replace=False)
+        X_num_fake = X_num_fake[ixs]
+        X_cat_fake = X_cat_fake[ixs] if X_cat_fake is not None else None
+
+
+    mm = MinMaxScaler().fit(X_num_real)
+    X_real = mm.transform(X_num_real)
+    X_fake = mm.transform(X_num_fake)
+    if X_cat_real is not None:
+        ohe = OneHotEncoder().fit(X_cat_real)
+        X_cat_real = ohe.transform(X_cat_real) / np.sqrt(2)
+        X_cat_fake = ohe.transform(X_cat_fake) / np.sqrt(2)
+
+        X_real = np.concatenate([X_real, X_cat_real.todense()], axis=1)
+        X_fake = np.concatenate([X_fake, X_cat_fake.todense()], axis=1)
+
+    # Computing pair-wise distances between real and synthetic 
+    dist_rf = pairwise_distances(X_fake, Y=X_real, metric='l2', n_jobs=-1)
+    
+    # Computing first and second smallest nearest neighbour distances between real and synthetic
+    smallest_two_indexes_rf = [dist_rf[i].argsort()[:2] for i in range(len(dist_rf))]
+    smallest_two_rf = [dist_rf[i][smallest_two_indexes_rf[i]] for i in range(len(dist_rf))]
+
+    # Computing 5th percentiles for DCR and NNDR between and within real and synthetic datasets
+    min_dist_rf = np.array([i[0] for i in smallest_two_rf])
+    fifth_perc_rf = np.percentile(min_dist_rf,5)
+    return min_dist_rf 
