a90ec22db62f2243e5c1a45441de1b655d3efdd5
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2024-01-03 05:20:33 +0700

    Update treatment.py

--- ml_utility_loss/synthesizers/tab_ddpm/params2/treatment.py
+++ ml_utility_loss/synthesizers/tab_ddpm/params2/treatment.py
@@ -1,7 +1,7 @@
 PARAM_SPACE = {
     "n_samples": ("int_exp_2", 16, 256),
     #"sample_batch_size": ("int_exp_2", 256, 1024),
-    "t_steps": ("int_exp_2", 256, 1024),
+    "t_steps": ("int_exp_2", 256, 2048),
     "mlu_target": ("categorical", [None, 1.0]),
     "n_steps": ("int", 1, 4),
     "loss_fn": ("loss", [
