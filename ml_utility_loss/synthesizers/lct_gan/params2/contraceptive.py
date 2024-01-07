9b89a01c96b2d07e6b2746764c76c488674ac5f1
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2024-01-08 03:14:23 +0700

    Update contraceptive.py

--- ml_utility_loss/synthesizers/lct_gan/params2/contraceptive.py
+++ ml_utility_loss/synthesizers/lct_gan/params2/contraceptive.py
@@ -1,26 +1,23 @@
 PARAM_SPACE = {
-    "n_samples": ("int_exp_2", 16, 128),
+    "n_samples": ("int_exp_2", 16, 2048),
     #"sample_batch_size": ("int_exp_2", 16, 512),
-    "t_steps": ("int", 4, 10),
-    "mlu_target": ("categorical", [
-        #None, 
-        1.0
-    ]),
-    "n_steps": ("int", 2, 4),
+    "t_steps": ("int", 4, 16),
+    "mlu_target": ("categorical", [None, 1.0]),
+    "n_steps": ("int", 1, 4),
     "loss_fn": ("loss", [
         "mse",
-        #"mae",
+        "mae",
         "mile",
         "mire",
     ]),
-    "loss_mul": ("log_float", 1e-3, 0.1),
+    "loss_mul": 1,
     "Optim": ("optimizer", [
         "adamw",  
         "amsgradw",
-        #"adamp",
-        #"diffgrad",
+        "adamp",
+        "diffgrad",
     ]),
-    "mlu_lr": ("log_float", 1e-6, 1e-5),
+    "mlu_lr": ("log_float", 1e-6, 1e-3),
 }
 #85
 #0.47745077926555196
