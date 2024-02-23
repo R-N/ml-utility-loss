65496580f849c217521dc2f88562a17a80991679
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2024-02-24 00:40:36 +0700

    Revert "bias"
    This reverts commit a362da0e537719d172e8cf1e57353e090609378f.

--- ml_utility_loss/loss_learning/estimator/params2/insurance.py
+++ ml_utility_loss/loss_learning/estimator/params2/insurance.py
@@ -101,8 +101,8 @@ PARAM_SPACE = {
     "d_model": ("int_exp_2", 16, 512), 
     #"dropout": ("bool_float", 0.15, 0.5), 
     "grad_clip": ("log_float", 0.55, 1.0),
-    "bias": BOOLEAN,
-    "bias_final": BOOLEAN,
+    #"bias": BOOLEAN,
+    #"bias_final": BOOLEAN,
     #"pma_layer_norm": BOOLEAN,
     "attn_activation": ("activation", [
         "tanh",  
