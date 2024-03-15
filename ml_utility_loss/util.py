1a54a2fb3aff1d8ada79ed1a87974140e4dc8469
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2024-03-16 03:12:43 +0700

    Update util.py

--- ml_utility_loss/util.py
+++ ml_utility_loss/util.py
@@ -57,21 +57,22 @@ def split_df_2(df, points, test=-1, val=None, return_3=False, **kwargs):
 
 def split_df_ratio(df, ratio=0.2, val=False, i=0, return_3=False, reverse_index=False, **kwargs):
     if ratio == 0 or ratio == 1:
-        train_df = df.iloc[:]
         if isinstance(df, pd.DataFrame):
             create_empty = lambda: pd.DataFrame(columns=list(df.columns))
         elif isinstance(df, pd.Series):
             create_empty = lambda: pd.Series()
-        test_df = create_empty()
-        val_df = test_df
-        if val:
-            val_df = create_empty()
-    elif ratio == 1:
-        train_df = create_empty()
-        test_df = df.iloc[:]
-        val_df = test_df
-        if val:
-            val_df = df.iloc[:]
+        if ratio == 0:
+            train_df = df.iloc[:]
+            test_df = create_empty()
+            val_df = test_df
+            if val:
+                val_df = create_empty()
+        elif ratio == 1:
+            train_df = create_empty()
+            test_df = df.iloc[:]
+            val_df = test_df
+            if val:
+                val_df = df.iloc[:]
     else:
         count = int(1.0/ratio)
         splits = [k*ratio for k in range(1, count)]
