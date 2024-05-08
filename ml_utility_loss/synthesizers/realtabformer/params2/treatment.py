c98f5a4c9695bde6f8d89ea669110d85fe807596
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2024-05-08 13:21:54 +0700

    Update treatment.py

--- ml_utility_loss/synthesizers/realtabformer/params2/treatment.py
+++ ml_utility_loss/synthesizers/realtabformer/params2/treatment.py
@@ -400,46 +400,46 @@ add_queue(BEST_GP_MUL)
 # #mlu target true
 # #47
 # #0.620780635285055
-# BEST_GP_MUL = {
-#     't_start_bool': False,
-#     't_end_bool': False,
-#     'mlu_target_bool': True,
-#     'mlu_target': 0.6749999999999999,
-#     'n_steps': 1,
-#     'n_inner_steps_exp_2': 0,
-#     'n_inner_steps_2_exp_2': 0,
-#     'div_batch': True,
-#     'mlu_loss_fn': 'mae',
-#     'n_real_bool': False,
-#     'n_samples_exp_2': 4,
-#     't_steps': 4,
-#     'mlu_Optim': 'adamp',
-#     'mlu_lr': 0.002263302939027342,
-#     'mlu_run': 3,
-# }
+add_queue({
+    't_start_bool': False,
+    't_end_bool': False,
+    'mlu_target_bool': True,
+    'mlu_target': 0.6749999999999999,
+    'n_steps': 1,
+    'n_inner_steps_exp_2': 0,
+    'n_inner_steps_2_exp_2': 0,
+    'div_batch': True,
+    'mlu_loss_fn': 'mae',
+    'n_real_bool': False,
+    'n_samples_exp_2': 4,
+    't_steps': 4,
+    'mlu_Optim': 'adamp',
+    'mlu_lr': 0.002263302939027342,
+    'mlu_run': 3,
+})
 # add_queue(BEST_GP_MUL)
 
 # #diff
 # #24
 # #0.6151384694832414
-# BEST_GP_MUL = {
-#     't_start_bool': False,
-#     't_end_bool': True,
-#     't_end': 80,
-#     'mlu_target': 0.08,
-#     'n_steps': 1,
-#     'n_inner_steps_exp_2': 0,
-#     'n_inner_steps_2_exp_2': 0,
-#     'div_batch': False,
-#     'mlu_loss_fn': 'mae',
-#     'n_real_bool': True,
-#     'n_real_exp_2': 8,
-#     'n_samples_exp_2': 3,
-#     't_steps': 4,
-#     'mlu_Optim': 'adamp',
-#     'mlu_lr': 0.0009842992255082153,
-#     'mlu_run': 3,
-# }
+add_queue({
+    't_start_bool': False,
+    't_end_bool': True,
+    't_end': 80,
+    'mlu_target': 0.08,
+    'n_steps': 1,
+    'n_inner_steps_exp_2': 0,
+    'n_inner_steps_2_exp_2': 0,
+    'div_batch': False,
+    'mlu_loss_fn': 'mae',
+    'n_real_bool': True,
+    'n_real_exp_2': 8,
+    'n_samples_exp_2': 3,
+    't_steps': 4,
+    'mlu_Optim': 'adamp',
+    'mlu_lr': 0.0009842992255082153,
+    'mlu_run': 3,
+})
 # add_queue(BEST_GP_MUL)
 
 BEST_DICT = {
